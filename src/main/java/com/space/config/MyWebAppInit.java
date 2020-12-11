84aa4280774f59ace31d7ff5043bf2074503e461
source-store <73580046+source-store@users.noreply.github.com>
2020-12-11 16:34:14 +0300

    begin

--- src/main/java/com/space/config/MyWebAppInit.java
+++ src/main/java/com/space/config/MyWebAppInit.java
@@ -1,21 +1,18 @@
 package com.space.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 public class MyWebAppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-/*
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         super.onStartup(servletContext);
         servletContext.setInitParameter("spring.profiles.active", "prod");
     }
-*/
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{AppConfig.class};
